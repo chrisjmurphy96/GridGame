3836d7b9e46b38a84833863fbd04cfc00dd01bd6
Chris Murphy <chrisjmurphy96@gmail.com>
2026-05-12 10:08:04 -0500

    Missed changes to move LdtkEntityInstance out of LdtkLayerInstance.cs

--- GridGame/GridLibrary/Ldtk/LdtkLayerInstance.cs
+++ GridGame/GridLibrary/Ldtk/LdtkLayerInstance.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Text.Json.Serialization;
-using Microsoft.Xna.Framework;
 
 namespace GridLibrary.Ldtk;
 
@@ -31,19 +30,4 @@ public struct LdtkLayerInstance
 
     // Entity layer only
     public LdtkEntityInstance[] EntityInstances { get; set; }
-}
-
-public struct LdtkEntityInstance
-{
-    [JsonPropertyName("__identifier")]
-    public string Identifier { get; set; }
-    // Might be worth making a custom converter to turn these obnoxious
-    // arrays directly into Points
-    [JsonPropertyName("__grid")]
-    public int[] Grid { get; set; }
-
-    public readonly Point Position => new(Grid[0], Grid[1]);
-
-    // I could have the texture coordinates be part of custom data.
-    // Maybe if it gets annoying managing it in the codebase.
 }
